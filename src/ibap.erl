0e822cf85fd949d34885b071726ffc97f9fc1ef4
thanosqr <thanosqr@gmail.com>
2012-02-20 15:59:58 +0200

    trace analyzing: generating zoom levels

--- src/ibap.erl
+++ src/ibap.erl
@@ -1,18 +1,31 @@
--module(ibag).
+-module(ibap).
 -compile(export_all).
 -record(bytes,{file,
 	       data,
-	       core=0,
 	       left}).
+
+-record(in,{value,
+	    left,
+	    file,
+	    ones}).
+
+-record(out,{file,
+	     zoom,
+	     coreID,
+	     data,
+	     size,
+	     timestart,
+	     datalength=0}).
+
+-define(MAX_OUT,1000).
+-define(READ_N,1000000).
+-define(MAX_DUR,63).
+-define(MAX_TIME,255).
 	       
 get_packet(Bytes)->
     case get_byte(Bytes) of
-	{<<0:8>>,Bytes2} ->
-	    Bytes3 = Bytes2#bytes{core=Bytes2#bytes.core+1},
-	    get_packet(Bytes3);
-	{<<1:1,0:7>>,Bytes2} ->
-	    Bytes3 = Bytes2#bytes{core=0},
-	    get_packet(Bytes3);
+	end_of_file ->
+	    end_of_file;
 	{Duration1,Bytes2}->
 	    {Duration,Fo,Fm,Bytes3}=get_duration(Duration1,Bytes2),
 	    {Time,Bytes4} = get_time(Bytes3),
@@ -22,20 +35,25 @@ get_packet(Bytes)->
     end.
 
 get_byte(Bytes)->
+%    io:write(Bytes#bytes.file),io:nl(),
     case Bytes#bytes.left of
 	0 ->
-	    get_byte(refill(Bytes));
+	    case file:read(Bytes#bytes.file,?READ_N) of
+		{ok,[D|Data]}->
+		    {D,Bytes#bytes{left=erlang:length(Data),data=Data}};
+		eof -> 
+		    io:write(lala),
+		    file:close(Bytes#bytes.file),
+		    end_of_file
+	    end;
 	N ->
-	    <<Byte:8,Rest/Binary>> = Bytes#bytes.data,
-	    {Byte, Bytes#byte{data=Rest,left=N-1}}
+	    [Byte|Rest] = Bytes#bytes.data,
+	    {Byte, Bytes#bytes{data=Rest,left=N-1}}
     end.
 
-refill(Bytes)->
-    Bytes#bytes{left=?READ_N,
-		data=file:read(Bytes#bytes.file,?READ_N)}.
 
 get_duration(Duration1,Bytes2)->    	       
-    <<Fo:1,Fm:1,Duration>> = Duration1,
+    <<Fo:1,Fm:1,Duration:6>> = <<Duration1>>,
     if Duration<?MAX_DUR->
 	    {Duration,Fo,Fm,Bytes2};
        Duration==?MAX_DUR ->
@@ -62,8 +80,8 @@ get_time(Bytes)->
 
 get_pid(Bytes)->
     {B1,Bytes1} = get_byte(Bytes),
-    {B2,Bytes2} = get_byte(Bytes),
-    <<_:1,PID:15>> = <<Bytes1:8,Bytes2:8>>,
+    {B2,Bytes2} = get_byte(Bytes1),
+    <<_:1,PID:15>> = <<B1:8,B2:8>>,
     {<<0:1,PID:15>>,Bytes2}.
 
 get_mfa(Fo,Fm,Bytes)->
@@ -93,28 +111,142 @@ get_times(Bytes)->
     get_packet(Bytes).
 
 
-get_unit(CurrentTime,Times,Bytes)->
-    get_unit(CurrentTime,Times,Bytes,?UNIT_SIZE,0)
-get_unit(CurrentTime,Times,Bytes,0,Acc)->
-    {Acc/?UNIT_SIZE,CurrentTime,{TimeStart,Duration},Bytes};
-get_unit(CurrentTime,Times,Bytes,N,Acc) -> 
-    case get_one(CurrentTime,Times,Bytes) of
-	end_of_file->
-	    end_of_file;
-	{Val,NCurrentTime,NTimes,NBytes} ->
-	    get_one(NCurrentTime,NTimes,NBytes,N-1,Acc+Val)
+calc_val(end_of_file,_)->
+    end_of_file;
+calc_val(In,GU)->
+    {Val,In2}=calc_val(In,GU,0),
+    {Val,In2}.
+
+calc_val(end_of_file,_N,Acc)->
+    {Acc,end_of_file};    
+calc_val(In,N,Acc)->
+    case get_values(In,N) of
+	{N2,NVal,In2}->
+	    calc_val(refill(In2),N2,Acc+NVal);
+	{Val,In2} ->
+		{Acc+Val,In2}
     end.
-get_one(CurrentTime,{TimeStart,Duration},Bytes)->
-    if CurrentTime<TimeStart->
-	    {0,CurrentTime+1,{TimeStart,Duration}};
-       (CurrentTime>=Timestart) and (Duration>0)->
-	    {1,CurrentTime+1,{TimeStart,Duration-1}};
-       Duration==0->
-	    =get_times(Bytes),
-	    case get_times(Bytes) of
-		end_of_file->
+
+
+get_values(In,N)->
+    Left = In#in.left,
+    if Left>=N ->
+	    {N*In#in.value,In#in{left=Left-N}};
+       Left<N ->
+	    {N-Left,(N-Left)*In#in.value,In#in{left=0}}
+    end.
+
+refill(In)->
+    case In#in.value of
+	1 ->
+	    case get_times(In#in.file) of
+		end_of_file -> 
 		    end_of_file;
-		{NTimeStart,NDuration,NBytes} ->
-		    get_one(CurrentTime,{NTimeStart,NDuration},NBytes)
-	    end
+		{Bytes,{Time,Duration}}->
+		    In#in{file=Bytes,
+			  left=Time,
+			  value=0,
+			  ones=Duration}
+	    end;
+	0 ->
+	    In#in{left=In#in.ones,
+		  value=1}
     end.
+
+
+decode(In,Out,GU)->
+    case calc_val(In,GU) of
+	end_of_file->
+	    close_out(Out);
+	{Val,In2} ->
+	    Out2=insert(Out,Val),
+	    decode(In2,Out2,GU)
+    end.
+
+insert(Out,Val)->
+    Size = Out#out.size,
+    if Size > ?MAX_OUT ->
+	    save(Out);
+        Size =< ?MAX_OUT->
+	    Out#out{data=[Val|Out#out.data]}
+    end.
+
+save(Out)->
+    dets:insert(Out#out.file,{{Out#out.coreID, Out#out.zoom, Out#out.timestart},Out#out.data}),
+    Out#out{data=[],timestart=Out#out.timestart+?MAX_OUT, datalength=Out#out.datalength+length(Out#out.data)}.
+
+
+close_out(Out)->
+    dets:close(save(Out)),
+    Out#out.datalength+length(Out#out.data).
+
+open_out(OutName,CoreID)->
+    {ok,File}=dets:open_file(OutName,[]),
+    #out{file=File,
+	     zoom=0,
+	     coreID=CoreID,
+	     data=[],
+	     size=0,
+	     timestart=0}.
+
+open_in(InName,CoreID)->
+    IName=string:concat(atom_to_list(InName),integer_to_list(CoreID)),
+    {ok,File}=file:open(IName,[read,raw,compressed]),
+    #in{file=#bytes{file=File,data=[],left=0},
+	left=0,
+	value=1,
+	ones=0}.
+    
+decoder(OutName,InName,CoreID,GU,PID)->
+    Out=open_out(OutName,CoreID),
+    In =open_in(InName,CoreID), 
+    PID!decode(In,Out,GU).
+    
+
+decode_all()->
+    decode_all(decoded,trace_gabi,8,{1,6}).
+decode_all(OutName,InName,GU,{FromCore,ToCore})->
+    lists:map(fun(CoreID)->
+		      spawn(ibap,decoder,[OutName,InName,CoreID,GU,self()])
+	      end, lists:seq(FromCore,ToCore)),
+    lists:map(fun(_)->
+		      receive
+			  done->
+			      ok
+		      end end, lists:seq(FromCore,ToCore)).
+    
+
+
+generate_zoom_lvls(Dets,{FromCore,ToCore},MaxZoomOut)->
+    lists:map(fun(OldZoom)->
+		      lists:map(fun(CoreID)->
+					Keys=dets:match(Dets,{{CoreID,OldZoom,'$3'},'_'}),
+					SKeys=lists:sort(Keys),
+					traverse(Dets,CoreID,OldZoom,SKeys)
+				end, lists:seq(FromCore,ToCore))
+	      end, lists:seq(1,MaxZoomOut)).
+
+traverse(Dets,CoreID,OldZoom,[TimeIn1,TimeIn2|Keys])->
+    [Values1]=dets:lookup(Dets,{CoreID,OldZoom,TimeIn1}),
+    [Values2]=dets:lookup(Dets,{CoreID,OldZoom,TimeIn2}),
+    Values=lists:zipwith(fun(X,Y)->(X+Y) div 2 end, Values1,Values2),
+    dets:insert(Dets,{{CoreID,OldZoom+1,TimeIn1},Values}),
+    traverse(Dets,CoreID,OldZoom,Keys);
+traverse(_Dets,_CoreID,_OldZoom,_Keys) ->
+    ok.
+
+    
+    
+
+analyze(InName,OutName,GU,{FromCore,ToCore})->
+    Longest=decode_all(OutName,InName,GU,{FromCore,ToCore}),
+    MaxZoomLevel=erlang:trunc(math:log(Longest)/math:log(2))+1,
+    {ok,Dets}=dets:open_file(OutName),
+    generate_zoom_lvls(Dets,{FromCore,ToCore},MaxZoomLevel),
+    dets:insert(Dets,{cores,ToCore-FromCore+1}),
+    dets:close(Dets);
+analyze(InName,OutName,GU,CoreN)->
+    analyze(InName,OutName,GU,{1,CoreN}).
+   
+analyze(InName,OutName)->
+    analyze(InName,OutName,8,{1,6}).
